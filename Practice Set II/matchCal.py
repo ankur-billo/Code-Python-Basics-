6790c1bd5900c38a93afd6dd47cb80aca5256804
Ankur Sardar <ankursardar.work@gmail.com>
2026-02-26 22:42:13 +0530

    first python commit

---
+++ Practice Set II/matchCal.py
@@ -0,0 +1,14 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+op = input("Enter the operation to be performed: ")
+match op:
+    case "+" :
+        print(a,"+",b,"=",a+b)
+    case "-" :
+        print(a,"-",b,"=",a-b)
+    case "/" :
+        print(a,"/",b,"=",a/b)
+    case "*" :
+        print(a,"*",b,"=",a*b)
+    case _:
+        print("Wrong Input!")
