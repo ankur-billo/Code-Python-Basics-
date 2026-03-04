0bb188f3e39c3e3364a5f547d207e0fe16e34425
Ankur Sardar <ankursardar.work@gmail.com>
2026-03-04 11:56:46 +0530

    articles not working FAAHHHH

--- newsAPI/main.py
+++ newsAPI/main.py
@@ -4,4 +4,14 @@ query = input("What is the topic you want to read ?\n")
 api="e7b4c265317245e9b550c89dee33170b"
 
 url=f"https://newsapi.org/v2/everything?q={query}&from=2026-02-03&sortBy=publishedAt&apiKey={api}"
+
+
 print(url)
+r =  requests.get(url)
+
+data = r.json()
+articles = data["articles"]
+
+for index, article in enumerate(articles):
+    print(index + 1, article["title"], article["url"])
+    print("\n****************************************\n")
