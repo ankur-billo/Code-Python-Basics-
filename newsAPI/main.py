b1ddc4d5f9622f7b67607bfb4349ef4af9b9b0e8
Ankur Sardar <ankursardar.work@gmail.com>
2026-03-05 20:21:27 +0530

    done

--- newsAPI/main.py
+++ newsAPI/main.py
@@ -1,9 +1,13 @@
 import requests
 
-# query = input("What is the topic you want to read ?\n")
+sources= input("What is the topic you want to read ?\n")
 # api="e7b4c265317245e9b550c89dee33170b"
 
-url="https://newsapi.org/v2/everything?q=tesla&from=2026-02-04&sortBy=publishedAt&apiKey=e7b4c265317245e9b550c89dee33170b"
+# url="https://newsapi.org/v2/everything?q=tesla&from=2026-02-04&sortBy=publishedAt&apiKey=e7b4c265317245e9b550c89dee33170b"
+
+# url=f"https://newsapi.org/v2/top-headlines?sources={sources}&apiKey=e7b4c265317245e9b550c89dee33170b"
+
+url =f"https://newsapi.org/v2/everything?q={sources}&from=2026-02-05&sortBy=publishedAt&apiKey=e7b4c265317245e9b550c89dee33170b"
 
 
 print(url)
