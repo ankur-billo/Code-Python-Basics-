ab1aa2a798ca7128e76cd7351d5a28e4b571cc69
Ankur Sardar <ankursardar.work@gmail.com>
2026-03-05 20:53:09 +0530

    babachu reminder

---
+++ drink water reminder/main.py
@@ -0,0 +1,19 @@
+'''
+BABACHUU MAC CHU CHU HOGYA THA ISKI MAA KAA
+'''
+# # import time
+# from plyer import notification
+# while True:
+#     # print("Please Drink Water!")
+#     notification.notify(title="Please Drink Some Water",
+#                          message="You need to Drink Some Water!",
+#                          timeout=10
+#                          )
+
+
+# from pync import Notifier
+# while True:
+#     Notifier.notify(title="Please Drink SOme Water!",
+#                     message="You need to Drink Some Water!")
+    
+#     # time.sleep(3) # delay timer
