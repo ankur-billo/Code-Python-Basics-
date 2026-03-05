01787001e42b35400687b47a20ede81e66cc9623
Ankur Sardar <ankursardar.work@gmail.com>
2026-03-05 21:57:10 +0530

    PAUSE AI PERSONALISED AGENT

---
+++ personal ai/main.py
@@ -0,0 +1,22 @@
+
+import os
+from openai import OpenAI
+
+key="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"
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=key,
+)
+
+def completion(message):
+    chat_completion = client.chat.completions.create(
+    model="gpt-5.2",
+    messages=[
+        {
+            "role": "user",
+            "content": message,
+        },
+    ],
+)
+
